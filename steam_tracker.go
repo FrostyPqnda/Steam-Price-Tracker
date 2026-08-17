03a6d10b5c165e6873bd744c979d993fdba9e0cb
Brian Pham <frostypqnda@gmail.com>
2026-08-17 15:05:18 -0500

    feat(mongodb integration): add upserting into mongodb

--- steam_tracker.go
+++ steam_tracker.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"log"
-	"os"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 type PriceRecord struct {
-	//AppId         int       `bson:"app_id" json:"app_id"`
 	OriginalPrice float64   `bson:"original_price" json:"original_price"`
 	DiscountPrice float64   `bson:"discount_price" json:"discount_price"`
 	Discount      float64   `bson:"discount" json:"discount"`
@@ -21,15 +23,70 @@ type PriceRecord struct {
 }
 
 type Game struct {
-	AppId        int           `bson:"app_id" json:"app_id"`
-	Title        string        `bson:"title" json:"title"`
-	URL          string        `bson:"url" json:"url"`
-	PriceHistory []PriceRecord `bson:"price_history" json:"price_history"`
+	AppId         int           `bson:"_id" json:"app_id"`
+	Title         string        `bson:"title" json:"title"`
+	URL           string        `bson:"url" json:"url"`
+	PriceSnapshot PriceRecord   `bson:"price_snapshot" json:"price_snapshot"`
+	PriceHistory  []PriceRecord `bson:"price_history" json:"price_history"`
 }
 
 var games []Game
 
-func parse_pagination_data(totalGames, totalPages *int) {
+func connectMongoDB() (*mongo.Client, error) {
+	client, err := mongo.Connect(
+		options.Client().ApplyURI("mongodb://localhost:27017/"),
+	)
+	if err != nil {
+
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		client.Disconnect(context.Background())
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func upsertGame(collection *mongo.Collection, g Game) error {
+	var existing Game
+	err := collection.FindOne(
+		context.TODO(),
+		bson.M{"_id": g.AppId},
+	).Decode(&existing)
+
+	if err == mongo.ErrNoDocuments {
+		_, insertErr := collection.InsertOne(context.TODO(), g)
+		return insertErr
+	}
+
+	if err != nil {
+		return err
+	}
+
+	rec := g.PriceSnapshot
+	last := existing.PriceHistory[len(existing.PriceHistory)-1]
+	changed := len(existing.PriceHistory) == 0 ||
+		last.DiscountPrice != rec.DiscountPrice ||
+		last.OriginalPrice != rec.OriginalPrice
+
+	update := bson.M{"$set": bson.M{
+		"price_snapshot": rec,
+		"title":          g.Title,
+		"url":            g.URL,
+	}}
+	if changed {
+		update["$push"] = bson.M{"price_history": rec}
+	}
+
+	_, err = collection.UpdateOne(context.TODO(), bson.M{"_id": g.AppId}, update)
+	return err
+}
+
+func parsePaginationData(totalGames, totalPages *int) {
 	c := colly.NewCollector()
 
 	c.OnHTML("div.search_pagination", func(e *colly.HTMLElement) {
@@ -69,7 +126,7 @@ func parse_pagination_data(totalGames, totalPages *int) {
 	}
 }
 
-func extract_value(s string) (float64, error) {
+func extractValue(s string) (float64, error) {
 	s = strings.TrimSpace(s)
 
 	if strings.HasSuffix(s, "%") {
@@ -85,9 +142,16 @@ func extract_value(s string) (float64, error) {
 	return 0, fmt.Errorf("unsupported value: %q", s)
 }
 
-func track(games *[]Game) {
+func cleanURL(href string) string {
+	if idx := strings.Index(href, "?"); idx != -1 {
+		return href[:idx]
+	}
+	return href
+}
+
+func track(collection *mongo.Collection) {
 	var totalGames, totalPages int
-	parse_pagination_data(&totalGames, &totalPages)
+	parsePaginationData(&totalGames, &totalPages)
 	fmt.Printf("total_games=%d, total_pages=%d\n", totalGames, totalPages)
 
 	c := colly.NewCollector(
@@ -95,75 +159,80 @@ func track(games *[]Game) {
 	)
 
 	c.Limit(&colly.LimitRule{
-		Delay:       60 * time.Second,
-		RandomDelay: 5 * time.Second,
+		Delay:       90 * time.Second,
+		RandomDelay: 30 * time.Second,
+	})
+	c.SetRequestTimeout(30 * time.Second)
+
+	c.OnHTML("div#search_resultsRows > a", func(e *colly.HTMLElement) {
+		discount, _ := extractValue(e.ChildText("div.discount_pct"))
+
+		original_price, _ := extractValue(e.ChildText("div.discount_original_price"))
+
+		discount_price, _ := extractValue(e.ChildText("div.discount_final_price"))
+
+		id, err := strconv.Atoi(e.Attr("data-ds-appid"))
+		if err != nil {
+			return
+		}
+
+		if e.DOM.Find("div.discount_block").HasClass("no_discount") {
+			original_price = discount_price
+			discount = 0
+		}
+
+		price := PriceRecord{
+			OriginalPrice: original_price,
+			DiscountPrice: discount_price,
+			Discount:      discount,
+			CheckedAt:     time.Now(),
+		}
+
+		game := Game{
+			AppId:         id,
+			Title:         e.ChildText("span.title"),
+			URL:           cleanURL(e.Attr("href")),
+			PriceSnapshot: price,
+			PriceHistory:  []PriceRecord{price},
+		}
+
+		if err := upsertGame(collection, game); err != nil {
+			fmt.Println("Upsert error:", err)
+		}
+
 	})
 
-	for page := 1; page <= 10; page++ {
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting:", r.URL)
+	})
 
-		c.OnHTML("div#search_resultsRows > a", func(e *colly.HTMLElement) {
-			discount, _ := extract_value(e.ChildText("div.discount_pct"))
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request error:", err)
+	})
 
-			original_price, _ := extract_value(e.ChildText("div.discount_original_price"))
-
-			discount_price, _ := extract_value(e.ChildText("div.discount_final_price"))
-
-			id, err := strconv.Atoi(e.Attr("data-ds-appid"))
-			if err != nil {
-				return
-			}
-
-			if e.DOM.Find("div.discount_block").HasClass("no_discount") {
-				original_price = discount_price
-				discount = 0
-			}
-
-			price := PriceRecord{
-				OriginalPrice: original_price,
-				DiscountPrice: discount_price,
-				Discount:      discount,
-				CheckedAt:     time.Now(),
-			}
-
-			game := Game{
-				AppId:        id,
-				Title:        e.ChildText("span.title"),
-				URL:          strings.TrimPrefix(e.Attr("href"), "?"),
-				PriceHistory: []PriceRecord{price},
-			}
-
-			*games = append(*games, game)
-		})
-
-		c.OnRequest(func(r *colly.Request) {
-			fmt.Println("Visiting:", r.URL)
-		})
-
-		c.OnError(func(r *colly.Response, err error) {
-			fmt.Println("Request error:", err)
-		})
-
+	for page := 1; page <= totalPages; page++ {
 		url := fmt.Sprintf("https://store.steampowered.com/search?hwtype=0&supportedlang=english&hidef2p=1&ndl=1&page=%d", page)
 		var err = c.Visit(url)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-}
 
-func writeJSON(games *[]Game) error {
-	data, err := json.MarshalIndent(games, "", "\t")
-	if err != nil {
-		return err
-	}
-
-	return os.WriteFile("games.json", data, 0644)
 }
 
 func main() {
-	track(&games)
-	err := writeJSON(&games)
+	client, err := connectMongoDB()
 	if err != nil {
-		panic(err)
+		log.Fatal("Could not connect to MongoDB:", err)
 	}
+
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			log.Println("MongoDB disconnect error:", err)
+		}
+	}()
+
+	collection := client.Database("steam_price_tracker").Collection("game")
+
+	track(collection)
 }
